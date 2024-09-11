2fb259b169b94e2df843b22be610e7d39911dff9
emmelykovalski <107261218+emmelykovalski@users.noreply.github.com>
2024-09-11 11:18:34 -0300

    11/09/2024

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ function diminuiTamanho(){
     tamanhoSenha--;
    }
    numeroSenha.textContent = tamanhoSenha;
+   geraSenha();
 }
 
 function aumentaTamanho(){
@@ -23,6 +24,49 @@ tamanhoSenha++;
 
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+    
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelector('.checkbox');
+
+for(i=0;i < checkbox.length; i++){
+checkbox[i].onclick = geraSenha;
+
 }
 
-console.log(Botoes);
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWYZ';
+
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+
+geraSenha();
+
+function geraSenha(){
+    {let alfabeto = '';
+    if (checkbox[0].checked){
+    alfabeto = alfabeto + letrasMaiusculas;
+}
+if (checkbox[0].checked){
+    alfabeto = alfabeto + letrasMinusculas;
+}
+if (checkbox[0].checked){
+    alfabeto = alfabeto + numeros;
+}
+if (checkbox[0].checked){
+    alfabeto = alfabeto + simbolos;
+}
+console.log(alfabeto);
+let senha = '';
+}
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha= senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+
+}
